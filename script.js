629ef9b1d53e271468547f0d9b3d8dfb38febdd0
CodeYourFuture <nawal.careem@yahoo.com>
2020-06-24 22:49:16 +0100

    added search bar

--- script.js
+++ script.js
@@ -2,22 +2,18 @@
 //You can edit ALL of the code here
 
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+const allEpisodes = getAllEpisodes();
+makePageForEpisodes(allEpisodes);
 }
 
-
-
-
-
-
- function makePageForEpisodes(allEpisodes) {
+function makePageForEpisodes(allEpisodes) {
   
-  allEpisodes.forEach((episode, index) => {
-  // Create the HTML elements needed for a card
+allEpisodes.forEach((episode, index) => {
+// Create the HTML elements needed for a card
 
 const div = document.createElement("div");
 const h4TagElem = document.createElement("h4");
+h4TagElem.classList = 'episode-info'
 const img = document.createElement("img");
 const pElem = document.createElement('p');
 
@@ -33,19 +29,50 @@ div.append(h4TagElem, img, pElem);
 //const showInfo = allEpisodes[0];
 
 // Use innerHTML to add text to the anchor tag
-h4TagElem.innerHTML = `${episode.name} `+  ` -S${episode.season.toString().padStart(2,'0')}E${episode.number.toString().padStart(2,'0')}`
+h4TagElem.innerHTML = `${episode.name}`
++  `-S${episode.season.toString().padStart(2,'0')}E${episode.number.toString().padStart(2,'0')}`
 
 pElem.innerHTML = episode.summary;
 // use setAttribute to add the card class to the div, etc
 
 //div.setAttribute("class", "card-group card img card-block card-title ");
 div.setAttribute("class", " main-wrapper card html body main-content card-block img");
+
 //div.setAttribute("class", "cards")
 img.setAttribute("src", episode.image.medium);
+
+
   
   });
  }
 
+ //filter episodes
+
+ const searchBar = document.querySelector('input');
+ searchBar.addEventListener('keyup', filterSearch);
+ let main = document.querySelector('#root')
+ 
+ function filterSearch(event) {
+ let firstValue = event.target.value.toLowerCase();
+ 
+ let episodes = [...main.children]  // making episodes into an array
+ episodes.forEach(show => {
+ const showText = show.innerText.toLowerCase()
+        if (showText.indexOf(firstValue) < 0) {
+          show.style.color = 'red'
+            
+        } else {
+            show.style.display = 'flex'
+        }
+    })
+  
+    let result = shows.filter(ep => getComputedStyle(ep).display == "flex")
+    displayNumber(shows, filteredEpisodes)
+  
+   
+
+
+  }
  
 
  window.onload = setup;
